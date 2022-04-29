3c0041040f2270f8d66d5b7d9348e64be92b61c3
tdorobantu <tdorobantu95@gmail.com>
2022-04-29 18:35:54 +0300

    Register page finished

--- controllers/user.js
+++ controllers/user.js
@@ -1,48 +1,75 @@
-import bcrypt from "bcrypt"
-import { getUser } from "../redis/user.js";
+import bcrypt from "bcrypt";
+import { getUser, registerUser } from "../redis/user.js";
 
 export const registerUserAPI = async (req, res) => {
-    
-    // declare regex patterns for string sanitization and unpack request body
-    const blacklistName = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
-    const blacklistEmail = /[` !#$%^&*()_+\-=[\]{};':"\\|,<>/?~]/g;
-    const { name, email, password } = req.body
-    
-    // sanitize req
-    const sanitizedName =  name.replace(blacklistName, '')
-    const sanitizedEmail = email.replace(blacklistEmail, '') 
-    //check if user with email already exists
-    try { 
-    const emailMatches = await getUser(sanitizedEmail)
-        if(emailMatches === 1) {
-            res.status(400).send({
-                email: "",
-                password: "",
-                name: "",
-                server: "A user with the same email address already exists! Try forgot password to reset password",
-              })
-        } else if (
-            emailMatches > 1
-        ) {
-            res.status(500).send({
-                email: "",
-                password: "",
-                name: "",
-                server: "Multiple user with the same email address already exists! Contact Admin!",
-              })
-        }
-    } catch (error) {
-        console.error(error)
-        res.status(500).send({
-            email: "",
-            password: "",
-            name: "",
-            server: "Redis db unavailable. Contact Admin!",
-          })
+  // declare regex patterns for string sanitization and unpack request body
+  const blacklistName = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+  const blacklistEmail = /[` !#$%^&*()_+\-=[\]{};':"\\|,<>/?~]/g;
+  const { name, email, password } = req.body;
+
+  // sanitize req
+  const sanitizedName = name.replace(blacklistName, "");
+  const sanitizedEmail = email.replace(blacklistEmail, "");
+  //check if user with email already exists
+  try {
+    const emailMatches = await getUser(sanitizedEmail);
+    if (emailMatches === 1) {
+      return res.status(400).json({
+        email: "",
+        password: "",
+        name: "",
+        server:
+          "A user with the same email address already exists! Try forgot password.",
+      });
+      return;
+    } else if (emailMatches > 1) {
+      return res.status(500).json({
+        email: "",
+        password: "",
+        name: "",
+        server:
+          "Multiple user with the same email address already exists! Contact Admin!",
+      });
     }
-    // Register user
-    
-        // if successful, confirm 
-        // else, send error.
-    res.status(200).send()
-}
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      email: "",
+      password: "",
+      name: "",
+      server: "Redis db failed user check. Contact Admin!",
+    });
+  }
+
+  let hashedPassword;
+
+  try {
+    hashedPassword = await bcrypt.hash(password, 10);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+
+  try {
+    const id = await registerUser({
+      name: sanitizedName,
+      email: sanitizedEmail,
+      password: hashedPassword,
+      unixJoin: Date.now(),
+      unixLastLogin: 0,
+      blackListed: false,
+    });
+    return res.status(200).send(id);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      email: "",
+      password: "",
+      name: "",
+      server: "Redis db failed user register. Contact Admin!",
+    });
+  }
+
+  // if successful, confirm
+  // else, send error.
+};
--- redis/user.js
+++ redis/user.js
@@ -1,12 +1,32 @@
-import { client, connect } from "./redis.js"
+import { client, connect } from "./redis.js";
 import schemaCampaign from "./models/campaign.js";
+import schemaUser from "./models/user.js";
 
 export const getUser = async (userEmail) => {
-    await connect(client)
+  await connect(client);
 
-    const repository = client.fetchRepository(schemaCampaign);
+  const repository = client.fetchRepository(schemaUser);
 
-    let match = await repository.search().where('email').equals(userEmail).returnCount()
+  console.log("userEmail >>>", userEmail);
 
-    return match
-}
+  let match = await repository
+    .search()
+    .where("email")
+    .equals(userEmail)
+    .returnCount();
+
+  console.log("getUser match >>>", match);
+
+  return match;
+};
+
+export const registerUser = async (data) => {
+  await connect(client);
+  console.log(data);
+
+  const repository = client.fetchRepository(schemaUser);
+
+  const id = await repository.createAndSave(data);
+
+  return id;
+};
